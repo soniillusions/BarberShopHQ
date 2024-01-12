4bedef43ebb572cd4584fb72409044a4382cedd9
Aleksey <alekseycrosso@gmail.com>
2024-01-12 17:28:24 +0300

    update from comments

--- app.rb
+++ app.rb
@@ -2,10 +2,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-require "sinatra/activerecord"
+require 'sinatra/activerecord'
 
-set :database, "sqlite3:barbershop.db"
-
+set :database, {adapter: "sqlite3", database: "barbershop.db"}
 class Client < ActiveRecord::Base
 end
 
