deeb9e237a15c754fbc0db6fd5a790b162c06788
Aleksey Maryshenkov <alekseycrosso@gmail.com>
2024-01-18 15:19:58 +0300

    add index.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_13_141447) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_18_114521) do
   create_table "barbers", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "client", force: :cascade do |t|
+  create_table "clients", force: :cascade do |t|
     t.text "name"
     t.text "phone"
     t.text "datestamp"
