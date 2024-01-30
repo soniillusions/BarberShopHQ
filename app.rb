4cd77a570c4d01a7582f1e22528e66467194e20e
Aleksey Maryshenkov <alekseycrosso@gmail.com>
2024-01-30 18:57:20 +0300

    Save values on post form

--- app.rb
+++ app.rb
@@ -28,16 +28,17 @@ get '/' do
 end
 
  get '/visit' do
+	 @c = Client.new
 	 erb :visit
  end
 
  post '/visit' do
 
-	 c = Client.new params[:client]
-	 if c.save
+	 @c = Client.new params[:client]
+	 if @c.save
 		 erb "Спасибо, вы записались"
 	 else
-		 @error = c.errors.full_messages.first
+		 @error = @c.errors.full_messages.first
 		 erb :visit
 	 end
  end
